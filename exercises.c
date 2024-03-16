a716e00cad8d282cd322dd39ab5b4c53d458fa37
Your Name <you@example.com>
2024-03-16 17:35:35 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -43,9 +43,8 @@ newsize apunta a una dirección válida que no ha sido inicializada con nigún v
 *newsize debe almacenar el tamaño del nuevo arreglo que se retorna.
 */
 int *filterEvenNumbers(int arr[], int size, int *newSize) {
-  int talla = 0;
   int *newArr = malloc(sizeof(int) * talla);
-  if(newArr == NULL) exit(EXIT_FAILURE);
+  int talla = 0;
   for(int i = 0; i < size; i++)
     {
       if(arr[i] % 2 == 0)
