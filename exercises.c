7f50b18c902f1f498440fff148c3099854dc5279
Your Name <you@example.com>
2024-03-16 16:50:59 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -45,11 +45,12 @@ newsize apunta a una dirección válida que no ha sido inicializada con nigún v
 int *filterEvenNumbers(int arr[], int size, int *newSize) {
   int talla = 0;
   int *newArr = malloc(sizeof(int) * talla);
+  if(newArr == NULL) exit(EXIT_FAILURE);
   for(int i = 0; i < size; i++)
     {
       if(arr[i] % 2 == 0)
       {
-        int *newArr = (int)realloc(newArr, sizeof(int) * (talla + 1));
+        *newArr = realloc(newArr, sizeof(int) * (talla + 1));
         if(newArr == NULL) exit(EXIT_FAILURE);
         newArr[talla] = arr[i];
         talla++;
